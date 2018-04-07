c2d00b1680238fa6f7624f9bfbfb2ac7494da5c1
raiyan106 <raiyan.khan.106@gmail.com>
2018-04-07 22:39:09 +0600

    Modified code along with second input string

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import sys
+import pprint as pp
 EOF='EOF'
 #Sample String
-string = "My Name is BLUE"
+#string = "My Name is BLUE and Green and yellow also it is a little bit of redish"
+
+#Reading From the File
+inputFile = sys.argv[1]
+file = open(inputFile,'r')
+string = file.read()
 
 #Creating the buffer and placing tokens in it
 
@@ -24,8 +30,36 @@ for index in range(len(buff)):
     buff[index]=listOfChar[i]
     i+=1
     
+#Dividing the Buffer into two part
+first=buff[0:len(buff)//2+1]
+#print(first)
+second=buff[len(buff)/2+1:len(buff)]
+#print(second)
 
-print(buff)
+#Tokenizing individual items from the buffers
 
+listOfTokens=[]
+value=''
+
+for index in range(len(first)-1):
+    if(first[index] is not ' '):
+        value+=first[index]
+    else:
+        listOfTokens.append(value)
+        value=''
+
+for index in range(len(second)-1):
+    if(second[index] is not ' '):
+        value+=second[index]
+    else:
+        listOfTokens.append(value)
+        value=''
+
+lastToken = string.split()
+lastToken = lastToken[len(lastToken)-1]
+listOfTokens.append(lastToken)
+#print(listOfTokens)
+#print(second)
+pp.pprint(listOfTokens,width=3)
 
 
--- play.py
+++ play.py
@@ -1,17 +1,4 @@
 file = open('input.txt','r')
 file = file.read()
-
-count=0
-
-for char in file:
-    if char is ' ':
-        count+=1
-
-#print(count)
-
-st = "My name is blue"
-
-lst = list(st)
-
-for i in range(10):
-    print(i)
+file = file.split()
+print(file)
